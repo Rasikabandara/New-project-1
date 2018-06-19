243275e5d2435eeb62f7831e61ab4c069dab11fb
Rasikabandara <40224370+Rasikabandara@users.noreply.github.com>
2018-06-19 09:50:21 +0530

    Add files via upload

---
+++ EX1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void main()
+	{
+        printf("University Of Moratuwa\n");
+	printf("Katubedda,\n");
+	printf("Moratuwa,\n");
+	printf("Sri Lanka\n");
+	printf("----------------\n");
+	printf("www.mrt.ac.lk\n");
+        }
